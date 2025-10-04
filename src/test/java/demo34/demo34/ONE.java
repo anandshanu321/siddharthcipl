8b53f9a32ca8cfd1396d8e6e749132f54c813b28
Shanu Anand <shanuanand411@gmail.com>
2025-10-04 17:43:20 +0530

    third

--- src/test/java/demo34/demo34/ONE.java
+++ src/test/java/demo34/demo34/ONE.java
@@ -3,10 +3,12 @@ package demo34.demo34;
 public class ONE {
 
 	public static void main(String[] args) {
-		System.out.println("shanu");
-		System.out.println("shanu");
-		System.out.println("shanu");
-		System.out.println("shanu");
+		System.out.println("anand");
+		System.out.println("anand");
+		System.out.println("anand");
+		System.out.println("anand");
+		System.out.println("anand");
+		
 
 	}
 
