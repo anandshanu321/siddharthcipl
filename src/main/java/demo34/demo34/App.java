0a84fbbb5885f9f177bcfca9ae555fcdf0668b3b
Shanu Anand <shanuanand411@gmail.com>
2025-10-04 17:29:08 +0530

    First commit

---
+++ src/main/java/demo34/demo34/App.java
@@ -0,0 +1,13 @@
+package demo34.demo34;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/demo34/demo34/ONE.java
@@ -0,0 +1,10 @@
+package demo34.demo34;
+
+public class ONE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
